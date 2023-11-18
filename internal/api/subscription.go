nilutz/miniflux-v2

commit 93e17c268f657a385a9ea26cc5750038624cd900

Refactor HTTP Client and LocalizedError packages

internal/api/subscription.go
@@ -32,7 +32,7 @@ func (h *handler) discoverSubscriptions(w http.ResponseWriter, r *http.Request) 
 		rssbridgeURL = intg.RSSBridgeURL
 	}
 
-	subscriptions, finderErr := subscription.FindSubscriptions(
+	subscriptions, localizedError := subscription.FindSubscriptions(
 		subscriptionDiscoveryRequest.URL,
 		subscriptionDiscoveryRequest.UserAgent,
 		subscriptionDiscoveryRequest.Cookie,
@@ -42,12 +42,13 @@ func (h *handler) discoverSubscriptions(w http.ResponseWriter, r *http.Request) 
 		subscriptionDiscoveryRequest.AllowSelfSignedCertificates,
 		rssbridgeURL,
 	)
-	if finderErr != nil {
-		json.ServerError(w, r, finderErr)
+
+	if localizedError != nil {
+		json.ServerError(w, r, localizedError.Error())
 		return
 	}
 
-	if subscriptions == nil {
+	if len(subscriptions) == 0 {
 		json.NotFound(w, r)
 		return
 	}

internal/reader/fetcher/request_builder.go
@@ -1,0 +1,168 @@
+// SPDX-FileCopyrightText: Copyright The Miniflux Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package fetcher // import "miniflux.app/v2/internal/reader/fetcher"
+
+import (
+	"crypto/tls"
+	"encoding/base64"
+	"log/slog"
+	"net"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const (
+	defaultHTTPClientTimeout     = 20
+	defaultHTTPClientMaxBodySize = 15 * 1024 * 1024
+)
+
+type RequestBuilder struct {
+	headers          http.Header
+	clientProxyURL   string
+	useClientProxy   bool
+	clientTimeout    int
+	withoutRedirects bool
+	ignoreTLSErrors  bool
+}
+
+func NewRequestBuilder() *RequestBuilder {
+	return &RequestBuilder{
+		headers:       make(http.Header),
+		clientTimeout: defaultHTTPClientTimeout,
+	}
+}
+
+func (r *RequestBuilder) WithHeader(key, value string) *RequestBuilder {
+	r.headers.Set(key, value)
+	return r
+}
+
+func (r *RequestBuilder) WithETag(etag string) *RequestBuilder {
+	if etag != "" {
+		r.headers.Set("If-None-Match", etag)
+	}
+	return r
+}
+
+func (r *RequestBuilder) WithLastModified(lastModified string) *RequestBuilder {
+	if lastModified != "" {
+		r.headers.Set("If-Modified-Since", lastModified)
+	}
+	return r
+}
+
+func (r *RequestBuilder) WithUserAgent(userAgent string) *RequestBuilder {
+	if userAgent != "" {
+		r.headers.Set("User-Agent", userAgent)
+	} else {
+		r.headers.Del("User-Agent")
+	}
+	return r
+}
+
+func (r *RequestBuilder) WithCookie(cookie string) *RequestBuilder {
+	if cookie != "" {
+		r.headers.Set("Cookie", cookie)
+	}
+	return r
+}
+
+func (r *RequestBuilder) WithUsernameAndPassword(username, password string) *RequestBuilder {
+	if username != "" && password != "" {
+		r.headers.Set("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(username+":"+password)))
+	}
+	return r
+}
+
+func (r *RequestBuilder) WithProxy(proxyURL string) *RequestBuilder {
+	r.clientProxyURL = proxyURL
+	return r
+}
+
+func (r *RequestBuilder) UseProxy(value bool) *RequestBuilder {
+	r.useClientProxy = value
+	return r
+}
+
+func (r *RequestBuilder) WithTimeout(timeout int) *RequestBuilder {
+	r.clientTimeout = timeout
+	return r
+}
+
+func (r *RequestBuilder) WithoutRedirects() *RequestBuilder {
+	r.withoutRedirects = true
+	return r
+}
+
+func (r *RequestBuilder) IgnoreTLSErrors(value bool) *RequestBuilder {
+	r.ignoreTLSErrors = value
+	return r
+}
+
+func (r *RequestBuilder) ExecuteRequest(requestURL string) (*http.Response, error) {
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			// Default is 30s.
+			Timeout: 10 * time.Second,
+
+			// Default is 30s.
+			KeepAlive: 15 * time.Second,
+		}).DialContext,
+
+		// Default is 100.
+		MaxIdleConns: 50,
+
+		// Default is 90s.
+		IdleConnTimeout: 10 * time.Second,
+
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: r.ignoreTLSErrors,
+		},
+	}
+
+	if r.useClientProxy && r.clientProxyURL != "" {
+		if proxyURL, err := url.Parse(r.clientProxyURL); err != nil {
+			slog.Warn("Unable to parse proxy URL",
+				slog.String("proxy_url", r.clientProxyURL),
+				slog.Any("error", err),
+			)
+		} else {
+			transport.Proxy = http.ProxyURL(proxyURL)
+		}
+	}
+
+	client := &http.Client{
+		Timeout: time.Duration(r.clientTimeout) * time.Second,
+	}
+
+	if r.withoutRedirects {
+		client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+	}
+
+	client.Transport = transport
+
+	req, err := http.NewRequest("GET", requestURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header = r.headers
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Connection", "close")
+
+	slog.Debug("Making outgoing request", slog.Group("request",
+		slog.String("method", req.Method),
+		slog.String("url", req.URL.String()),
+		slog.Any("headers", req.Header),
+		slog.Bool("without_redirects", r.withoutRedirects),
+		slog.Bool("with_proxy", r.useClientProxy),
+		slog.String("proxy_url", r.clientProxyURL),
+	))
+
+	return client.Do(req)
+}

internal/reader/handler/handler.go
@@ -4,16 +4,15 @@
 package handler // import "miniflux.app/v2/internal/reader/handler"
 
 import (
+	"errors"
 	"log/slog"
 	"time"
 
 	"miniflux.app/v2/internal/config"
-	"miniflux.app/v2/internal/errors"
-	"miniflux.app/v2/internal/http/client"
 	"miniflux.app/v2/internal/integration"
 	"miniflux.app/v2/internal/locale"
 	"miniflux.app/v2/internal/model"
-	"miniflux.app/v2/internal/reader/browser"
+	"miniflux.app/v2/internal/reader/fetcher"
 	"miniflux.app/v2/internal/reader/icon"
 	"miniflux.app/v2/internal/reader/parser"
 	"miniflux.app/v2/internal/reader/processor"
@@ -21,13 +20,13 @@ import (
 )
 
 var (
-	errDuplicate        = "This feed already exists (%s)"
-	errNotFound         = "Feed %d not found"
-	errCategoryNotFound = "Category not found for this user"
+	ErrCategoryNotFound = errors.New("fetcher: category not found")
+	ErrFeedNotFound     = errors.New("fetcher: feed not found")
+	ErrDuplicatedFeed   = errors.New("fetcher: duplicated feed")
 )
 
 // CreateFeed fetch, parse and store a new feed.
-func CreateFeed(store *storage.Storage, userID int64, feedCreationRequest *model.FeedCreationRequest) (*model.Feed, error) {
+func CreateFeed(store *storage.Storage, userID int64, feedCreationRequest *model.FeedCreationRequest) (*model.Feed, *locale.LocalizedErrorWrapper) {
 	slog.Debug("Begin feed creation process",
 		slog.Int64("user_id", userID),
 		slog.String("feed_url", feedCreationRequest.FeedURL),
@@ -35,35 +34,43 @@ func CreateFeed(store *storage.Storage, userID int64, feedCreationRequest *model
 
 	user, storeErr := store.UserByID(userID)
 	if storeErr != nil {
-		return nil, storeErr
+		return nil, locale.NewLocalizedErrorWrapper(storeErr, "error.database_error", storeErr)
 	}
 
 	if !store.CategoryIDExists(userID, feedCreationRequest.CategoryID) {
-		return nil, errors.NewLocalizedError(errCategoryNotFound)
+		return nil, locale.NewLocalizedErrorWrapper(ErrCategoryNotFound, "error.category_not_found")
 	}
 
-	request := client.NewClientWithConfig(feedCreationRequest.FeedURL, config.Opts)
-	request.WithCredentials(feedCreationRequest.Username, feedCreationRequest.Password)
-	request.WithUserAgent(feedCreationRequest.UserAgent)
-	request.WithCookie(feedCreationRequest.Cookie)
-	request.AllowSelfSignedCertificates = feedCreationRequest.AllowSelfSignedCertificates
+	requestBuilder := fetcher.NewRequestBuilder()
+	requestBuilder.WithUsernameAndPassword(feedCreationRequest.Username, feedCreationRequest.Password)
+	requestBuilder.WithUserAgent(feedCreationRequest.UserAgent)
+	requestBuilder.WithCookie(feedCreationRequest.Cookie)
+	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
+	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
+	requestBuilder.UseProxy(feedCreationRequest.FetchViaProxy)
+	requestBuilder.IgnoreTLSErrors(feedCreationRequest.AllowSelfSignedCertificates)
 
-	if feedCreationRequest.FetchViaProxy {
-		request.WithProxy()
+	responseHandler := fetcher.NewResponseHandler(requestBuilder.ExecuteRequest(feedCreationRequest.FeedURL))
+	defer responseHandler.Close()
+
+	if localizedError := responseHandler.LocalizedError(); localizedError != nil {
+		slog.Warn("Unable to fetch feed", slog.String("feed_url", feedCreationRequest.FeedURL), slog.Any("error", localizedError.Error()))
+		return nil, localizedError
 	}
 
-	response, requestErr := browser.Exec(request)
-	if requestErr != nil {
-		return nil, requestErr
+	responseBody, localizedError := responseHandler.ReadBody(config.Opts.HTTPClientMaxBodySize())
+	if localizedError != nil {
+		slog.Warn("Unable to fetch feed", slog.String("feed_url", feedCreationRequest.FeedURL), slog.Any("error", localizedError.Error()))
+		return nil, localizedError
 	}
 
-	if store.FeedURLExists(userID, response.EffectiveURL) {
-		return nil, errors.NewLocalizedError(errDuplicate, response.EffectiveURL)
+	if store.FeedURLExists(userID, responseHandler.EffectiveURL()) {
+		return nil, locale.NewLocalizedErrorWrapper(ErrDuplicatedFeed, "error.duplicated_feed")
 	}
 
-	subscription, parseErr := parser.ParseFeed(response.EffectiveURL, response.BodyAsString())
+	subscription, parseErr := parser.ParseFeed(responseHandler.EffectiveURL(), string(responseBody))
 	if parseErr != nil {
-		return nil, parseErr
+		return nil, locale.NewLocalizedErrorWrapper(parseErr, "error.unable_to_parse_feed", parseErr)
 	}
 
 	subscription.UserID = userID
@@ -81,14 +88,16 @@ func CreateFeed(store *storage.Storage, userID int64, feedCreationRequest *model
 	subscription.BlocklistRules = feedCreationRequest.BlocklistRules
 	subscription.KeeplistRules = feedCreationRequest.KeeplistRules
 	subscription.UrlRewriteRules = feedCreationRequest.UrlRewriteRules
+	subscription.EtagHeader = responseHandler.ETag()
+	subscription.LastModifiedHeader = responseHandler.LastModified()
+	subscription.FeedURL = responseHandler.EffectiveURL()
 	subscription.WithCategoryID(feedCreationRequest.CategoryID)
-	subscription.WithClientResponse(response)
 	subscription.CheckedNow()
 
 	processor.ProcessFeedEntries(store, subscription, user, true)
 
 	if storeErr := store.CreateFeed(subscription); storeErr != nil {
-		return nil, storeErr
+		return nil, locale.NewLocalizedErrorWrapper(storeErr, "error.database_error", storeErr)
 	}
 
 	slog.Debug("Created feed",
@@ -99,18 +108,16 @@ func CreateFeed(store *storage.Storage, userID int64, feedCreationRequest *model
 
 	checkFeedIcon(
 		store,
+		requestBuilder,
 		subscription.ID,
 		subscription.SiteURL,
 		subscription.IconURL,
-		feedCreationRequest.UserAgent,
-		feedCreationRequest.FetchViaProxy,
-		feedCreationRequest.AllowSelfSignedCertificates,
 	)
 	return subscription, nil
 }
 
 // RefreshFeed refreshes a feed.
-func RefreshFeed(store *storage.Storage, userID, feedID int64, forceRefresh bool) error {
+func RefreshFeed(store *storage.Storage, userID, feedID int64, forceRefresh bool) *locale.LocalizedErrorWrapper {
 	slog.Debug("Begin feed refresh process",
 		slog.Int64("user_id", userID),
 		slog.Int64("feed_id", feedID),
@@ -119,18 +126,16 @@ func RefreshFeed(store *storage.Storage, userID, feedID int64, forceRefresh bool
 
 	user, storeErr := store.UserByID(userID)
 	if storeErr != nil {
-		return storeErr
+		return locale.NewLocalizedErrorWrapper(storeErr, "error.database_error", storeErr)
 	}
-
-	printer := locale.NewPrinter(user.Language)
 
 	originalFeed, storeErr := store.FeedByID(userID, feedID)
 	if storeErr != nil {
-		return storeErr
+		return locale.NewLocalizedErrorWrapper(storeErr, "error.database_error", storeErr)
 	}
 
 	if originalFeed == nil {
-		return errors.NewLocalizedError(errNotFound, feedID)
+		return locale.NewLocalizedErrorWrapper(ErrFeedNotFound, "error.feed_not_found")
 	}
 
 	weeklyEntryCount := 0
@@ -138,52 +143,62 @@ func RefreshFeed(store *storage.Storage, userID, feedID int64, forceRefresh bool
 		var weeklyCountErr error
 		weeklyEntryCount, weeklyCountErr = store.WeeklyFeedEntryCount(userID, feedID)
 		if weeklyCountErr != nil {
-			return weeklyCountErr
+			return locale.NewLocalizedErrorWrapper(weeklyCountErr, "error.database_error", weeklyCountErr)
 		}
 	}
 
 	originalFeed.CheckedNow()
 	originalFeed.ScheduleNextCheck(weeklyEntryCount)
 
-	request := client.NewClientWithConfig(originalFeed.FeedURL, config.Opts)
-	request.WithCredentials(originalFeed.Username, originalFeed.Password)
-	request.WithUserAgent(originalFeed.UserAgent)
-	request.WithCookie(originalFeed.Cookie)
-	request.AllowSelfSignedCertificates = originalFeed.AllowSelfSignedCertificates
+	requestBuilder := fetcher.NewRequestBuilder()
+	requestBuilder.WithUsernameAndPassword(originalFeed.Username, originalFeed.Password)
+	requestBuilder.WithUserAgent(originalFeed.UserAgent)
+	requestBuilder.WithCookie(originalFeed.Cookie)
+	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
+	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
+	requestBuilder.UseProxy(originalFeed.FetchViaProxy)
+	requestBuilder.IgnoreTLSErrors(originalFeed.AllowSelfSignedCertificates)
 
-	if !originalFeed.IgnoreHTTPCache {
-		request.WithCacheHeaders(originalFeed.EtagHeader, originalFeed.LastModifiedHeader)
-	}
+	responseHandler := fetcher.NewResponseHandler(requestBuilder.ExecuteRequest(originalFeed.FeedURL))
+	defer responseHandler.Close()
 
-	if originalFeed.FetchViaProxy {
-		request.WithProxy()
-	}
-
-	response, requestErr := browser.Exec(request)
-	if requestErr != nil {
-		originalFeed.WithError(requestErr.Localize(printer))
+	if localizedError := responseHandler.LocalizedError(); localizedError != nil {
+		slog.Warn("Unable to fetch feed", slog.String("feed_url", originalFeed.FeedURL), slog.Any("error", localizedError.Error()))
+		originalFeed.WithTranslatedErrorMessage(localizedError.Translate(user.Language))
 		store.UpdateFeedError(originalFeed)
-		return requestErr
+		return localizedError
 	}
 
-	if store.AnotherFeedURLExists(userID, originalFeed.ID, response.EffectiveURL) {
-		storeErr := errors.NewLocalizedError(errDuplicate, response.EffectiveURL)
-		originalFeed.WithError(storeErr.Error())
+	if store.AnotherFeedURLExists(userID, originalFeed.ID, responseHandler.EffectiveURL()) {
+		localizedError := locale.NewLocalizedErrorWrapper(ErrDuplicatedFeed, "error.duplicated_feed")
+		originalFeed.WithTranslatedErrorMessage(localizedError.Translate(user.Language))
 		store.UpdateFeedError(originalFeed)
-		return storeErr
+		return localizedError
 	}
 
-	if originalFeed.IgnoreHTTPCache || response.IsModified(originalFeed.EtagHeader, originalFeed.LastModifiedHeader) {
+	if originalFeed.IgnoreHTTPCache || responseHandler.IsModified(originalFeed.EtagHeader, originalFeed.LastModifiedHeader) {
 		slog.Debug("Feed modified",
 			slog.Int64("user_id", userID),
 			slog.Int64("feed_id", feedID),
 		)
 
-		updatedFeed, parseErr := parser.ParseFeed(response.EffectiveURL, response.BodyAsString())
+		responseBody, localizedError := responseHandler.ReadBody(config.Opts.HTTPClientMaxBodySize())
+		if localizedError != nil {
+			slog.Warn("Unable to fetch feed", slog.String("feed_url", originalFeed.FeedURL), slog.Any("error", localizedError.Error()))
+			return localizedError
+		}
+
+		updatedFeed, parseErr := parser.ParseFeed(responseHandler.EffectiveURL(), string(responseBody))
 		if parseErr != nil {
-			originalFeed.WithError(parseErr.Localize(printer))
+			localizedError := locale.NewLocalizedErrorWrapper(parseErr, "error.unable_to_parse_feed")
+
+			if errors.Is(parseErr, parser.ErrFeedFormatNotDetected) {
+				localizedError = locale.NewLocalizedErrorWrapper(parseErr, "error.feed_format_not_detected", parseErr)
+			}
+
+			originalFeed.WithTranslatedErrorMessage(localizedError.Translate(user.Language))
 			store.UpdateFeedError(originalFeed)
-			return parseErr
+			return localizedError
 		}
 
 		// If the feed has a TTL defined, we use it to make sure we don't check it too often.
@@ -215,9 +230,10 @@ func RefreshFeed(store *storage.Storage, userID, feedID int64, forceRefresh bool
 		updateExistingEntries := forceRefresh || !originalFeed.Crawler
 		newEntries, storeErr := store.RefreshFeedEntries(originalFeed.UserID, originalFeed.ID, originalFeed.Entries, updateExistingEntries)
 		if storeErr != nil {
-			originalFeed.WithError(storeErr.Error())
+			localizedError := locale.NewLocalizedErrorWrapper(storeErr, "error.database_error", storeErr)
+			originalFeed.WithTranslatedErrorMessage(localizedError.Translate(user.Language))
 			store.UpdateFeedError(originalFeed)
-			return storeErr
+			return localizedError
 		}
 
 		userIntegrations, intErr := store.Integration(userID)
@@ -233,16 +249,15 @@ func RefreshFeed(store *storage.Storage, userID, feedID int64, forceRefresh bool
 
 		// We update caching headers only if the feed has been modified,
 		// because some websites don't return the same headers when replying with a 304.
-		originalFeed.WithClientResponse(response)
+		originalFeed.EtagHeader = responseHandler.ETag()
+		originalFeed.LastModifiedHeader = responseHandler.LastModified()
 
 		checkFeedIcon(
 			store,
+			requestBuilder,
 			originalFeed.ID,
 			originalFeed.SiteURL,
 			updatedFeed.IconURL,
-			originalFeed.UserAgent,
-			originalFeed.FetchViaProxy,
-			originalFeed.AllowSelfSignedCertificates,
 		)
 	} else {
 		slog.Debug("Feed not modified",
@@ -254,17 +269,18 @@ func RefreshFeed(store *storage.Storage, userID, feedID int64, forceRefresh bool
 	originalFeed.ResetErrorCounter()
 
 	if storeErr := store.UpdateFeed(originalFeed); storeErr != nil {
-		originalFeed.WithError(storeErr.Error())
+		localizedError := locale.NewLocalizedErrorWrapper(storeErr, "error.database_error", storeErr)
+		originalFeed.WithTranslatedErrorMessage(localizedError.Translate(user.Language))
 		store.UpdateFeedError(originalFeed)
-		return storeErr
+		return localizedError
 	}
 
 	return nil
 }
 
-func checkFeedIcon(store *storage.Storage, feedID int64, websiteURL, feedIconURL, userAgent string, fetchViaProxy, allowSelfSignedCertificates bool) {
+func checkFeedIcon(store *storage.Storage, requestBuilder *fetcher.RequestBuilder, feedID int64, websiteURL, feedIconURL string) {
 	if !store.HasIcon(feedID) {
-		iconFinder := icon.NewIconFinder(websiteURL, feedIconURL, userAgent, fetchViaProxy, allowSelfSignedCertificates)
+		iconFinder := icon.NewIconFinder(requestBuilder, websiteURL, feedIconURL)
 		if icon, err := iconFinder.FindIcon(); err != nil {
 			slog.Debug("Unable to find feed icon",
 				slog.Int64("feed_id", feedID),

internal/reader/processor/processor.go
@@ -12,10 +12,9 @@ import (
 	"time"
 
 	"miniflux.app/v2/internal/config"
-	"miniflux.app/v2/internal/http/client"
 	"miniflux.app/v2/internal/metric"
 	"miniflux.app/v2/internal/model"
-	"miniflux.app/v2/internal/reader/browser"
+	"miniflux.app/v2/internal/reader/fetcher"
 	"miniflux.app/v2/internal/reader/readingtime"
 	"miniflux.app/v2/internal/reader/rewrite"
 	"miniflux.app/v2/internal/reader/sanitizer"
@@ -52,7 +51,7 @@ func ProcessFeedEntries(store *storage.Storage, feed *model.Feed, user *model.Us
 			continue
 		}
 
-		url := getUrlFromEntry(feed, entry)
+		websiteURL := getUrlFromEntry(feed, entry)
 		entryIsNew := !store.EntryURLExists(feed.ID, entry.URL)
 		if feed.Crawler && (entryIsNew || forceRefresh) {
 			slog.Debug("Scraping entry",
@@ -64,13 +63,19 @@ func ProcessFeedEntries(store *storage.Storage, feed *model.Feed, user *model.Us
 			)
 
 			startTime := time.Now()
-			content, scraperErr := scraper.Fetch(
-				url,
+
+			requestBuilder := fetcher.NewRequestBuilder()
+			requestBuilder.WithUserAgent(feed.UserAgent)
+			requestBuilder.WithCookie(feed.Cookie)
+			requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
+			requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
+			requestBuilder.UseProxy(feed.FetchViaProxy)
+			requestBuilder.IgnoreTLSErrors(feed.AllowSelfSignedCertificates)
+
+			content, scraperErr := scraper.ScrapeWebsite(
+				requestBuilder,
+				websiteURL,
 				feed.ScraperRules,
-				feed.UserAgent,
-				feed.Cookie,
-				feed.AllowSelfSignedCertificates,
-				feed.FetchViaProxy,
 			)
 
 			if config.Opts.HasMetricsCollector() {
@@ -96,10 +101,10 @@ func ProcessFeedEntries(store *storage.Storage, feed *model.Feed, user *model.Us
 			}
 		}
 
-		rewrite.Rewriter(url, entry, feed.RewriteRules)
+		rewrite.Rewriter(websiteURL, entry, feed.RewriteRules)
 
 		// The sanitizer should always run at the end of the process to make sure unsafe HTML is filtered.
-		entry.Content = sanitizer.Sanitize(url, entry.Content)
+		entry.Content = sanitizer.Sanitize(websiteURL, entry.Content)
 
 		updateEntryReadingTime(store, feed, entry, entryIsNew, user)
 		filteredEntries = append(filteredEntries, entry)
@@ -146,15 +151,20 @@ func isAllowedEntry(feed *model.Feed, entry *model.Entry) bool {
 // ProcessEntryWebPage downloads the entry web page and apply rewrite rules.
 func ProcessEntryWebPage(feed *model.Feed, entry *model.Entry, user *model.User) error {
 	startTime := time.Now()
-	url := getUrlFromEntry(feed, entry)
+	websiteURL := getUrlFromEntry(feed, entry)
 
-	content, scraperErr := scraper.Fetch(
-		url,
-		entry.Feed.ScraperRules,
-		entry.Feed.UserAgent,
-		entry.Feed.Cookie,
-		feed.AllowSelfSignedCertificates,
-		feed.FetchViaProxy,
+	requestBuilder := fetcher.NewRequestBuilder()
+	requestBuilder.WithUserAgent(feed.UserAgent)
+	requestBuilder.WithCookie(feed.Cookie)
+	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
+	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
+	requestBuilder.UseProxy(feed.FetchViaProxy)
+	requestBuilder.IgnoreTLSErrors(feed.AllowSelfSignedCertificates)
+
+	content, scraperErr := scraper.ScrapeWebsite(
+		requestBuilder,
+		websiteURL,
+		feed.ScraperRules,
 	)
 
 	if config.Opts.HasMetricsCollector() {
@@ -174,8 +184,8 @@ func ProcessEntryWebPage(feed *model.Feed, entry *model.Entry, user *model.User)
 		entry.ReadingTime = readingtime.EstimateReadingTime(entry.Content, user.DefaultReadingSpeed, user.CJKReadingSpeed)
 	}
 
-	rewrite.Rewriter(url, entry, entry.Feed.RewriteRules)
-	entry.Content = sanitizer.Sanitize(url, entry.Content)
+	rewrite.Rewriter(websiteURL, entry, entry.Feed.RewriteRules)
+	entry.Content = sanitizer.Sanitize(websiteURL, entry.Content)
 
 	return nil
 }
@@ -270,14 +280,20 @@ func shouldFetchOdyseeWatchTime(entry *model.Entry) bool {
 	return matches != nil
 }
 
-func fetchYouTubeWatchTime(url string) (int, error) {
-	clt := client.NewClientWithConfig(url, config.Opts)
-	response, browserErr := browser.Exec(clt)
-	if browserErr != nil {
-		return 0, browserErr
+func fetchYouTubeWatchTime(websiteURL string) (int, error) {
+	requestBuilder := fetcher.NewRequestBuilder()
+	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
+	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
+
+	responseHandler := fetcher.NewResponseHandler(requestBuilder.ExecuteRequest(websiteURL))
+	defer responseHandler.Close()
+
+	if localizedError := responseHandler.LocalizedError(); localizedError != nil {
+		slog.Warn("Unable to fetch YouTube page", slog.String("website_url", websiteURL), slog.Any("error", localizedError.Error()))
+		return 0, localizedError.Error()
 	}
 
-	doc, docErr := goquery.NewDocumentFromReader(response.Body)
+	doc, docErr := goquery.NewDocumentFromReader(responseHandler.Body(config.Opts.HTTPClientMaxBodySize()))
 	if docErr != nil {
 		return 0, docErr
 	}
@@ -295,14 +311,20 @@ func fetchYouTubeWatchTime(url string) (int, error) {
 	return int(dur.Minutes()), nil
 }
 
-func fetchOdyseeWatchTime(url string) (int, error) {
-	clt := client.NewClientWithConfig(url, config.Opts)
-	response, browserErr := browser.Exec(clt)
-	if browserErr != nil {
-		return 0, browserErr
+func fetchOdyseeWatchTime(websiteURL string) (int, error) {
+	requestBuilder := fetcher.NewRequestBuilder()
+	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
+	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
+
+	responseHandler := fetcher.NewResponseHandler(requestBuilder.ExecuteRequest(websiteURL))
+	defer responseHandler.Close()
+
+	if localizedError := responseHandler.LocalizedError(); localizedError != nil {
+		slog.Warn("Unable to fetch Odysee watch time", slog.String("website_url", websiteURL), slog.Any("error", localizedError.Error()))
+		return 0, localizedError.Error()
 	}
 
-	doc, docErr := goquery.NewDocumentFromReader(response.Body)
+	doc, docErr := goquery.NewDocumentFromReader(responseHandler.Body(config.Opts.HTTPClientMaxBodySize()))
 	if docErr != nil {
 		return 0, docErr
 	}

commit ebbb289bc508619699e888534a92e9f0f557a1ab

Fix default User-Agent regression
The recent HTTP client refactor in 93e17c268f657a385a9ea26cc5750038624cd900
introduced a bug in which the global default User-Agent is no longer
used for requests. Unless a per-feed User-Agent exists, the Go standard
library's default User-Agent is used, which looks something like
"Go-http-client/1.1". To fix this, make RequestBuilder.WithUserAgent
take an additional argument, the default User-Agent, which will be used
if there is no per-feed User-Agent (i.e. it is an empty string).

Fixes #2188
Fixes #2189

internal/api/subscription.go
@@ -37,7 +37,7 @@ func (h *handler) discoverSubscriptions(w http.ResponseWriter, r *http.Request) 
 	requestBuilder := fetcher.NewRequestBuilder()
 	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
 	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
-	requestBuilder.WithUserAgent(subscriptionDiscoveryRequest.UserAgent)
+	requestBuilder.WithUserAgent(subscriptionDiscoveryRequest.UserAgent, config.Opts.HTTPClientUserAgent())
 	requestBuilder.WithCookie(subscriptionDiscoveryRequest.Cookie)
 	requestBuilder.WithUsernameAndPassword(subscriptionDiscoveryRequest.Username, subscriptionDiscoveryRequest.Password)
 	requestBuilder.UseProxy(subscriptionDiscoveryRequest.FetchViaProxy)

internal/reader/fetcher/request_builder.go
@@ -53,11 +53,11 @@ func (r *RequestBuilder) WithLastModified(lastModified string) *RequestBuilder {
 	return r
 }
 
-func (r *RequestBuilder) WithUserAgent(userAgent string) *RequestBuilder {
+func (r *RequestBuilder) WithUserAgent(userAgent string, defaultUserAgent string) *RequestBuilder {
 	if userAgent != "" {
 		r.headers.Set("User-Agent", userAgent)
 	} else {
-		r.headers.Del("User-Agent")
+		r.headers.Set("User-Agent", defaultUserAgent)
 	}
 	return r
 }

internal/reader/handler/handler.go
@@ -85,7 +85,7 @@ func CreateFeedFromSubscriptionDiscovery(store *storage.Storage, userID int64, f
 
 	requestBuilder := fetcher.NewRequestBuilder()
 	requestBuilder.WithUsernameAndPassword(feedCreationRequest.Username, feedCreationRequest.Password)
-	requestBuilder.WithUserAgent(feedCreationRequest.UserAgent)
+	requestBuilder.WithUserAgent(feedCreationRequest.UserAgent, config.Opts.HTTPClientUserAgent())
 	requestBuilder.WithCookie(feedCreationRequest.Cookie)
 	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
 	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
@@ -121,7 +121,7 @@ func CreateFeed(store *storage.Storage, userID int64, feedCreationRequest *model
 
 	requestBuilder := fetcher.NewRequestBuilder()
 	requestBuilder.WithUsernameAndPassword(feedCreationRequest.Username, feedCreationRequest.Password)
-	requestBuilder.WithUserAgent(feedCreationRequest.UserAgent)
+	requestBuilder.WithUserAgent(feedCreationRequest.UserAgent, config.Opts.HTTPClientUserAgent())
 	requestBuilder.WithCookie(feedCreationRequest.Cookie)
 	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
 	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
@@ -230,7 +230,7 @@ func RefreshFeed(store *storage.Storage, userID, feedID int64, forceRefresh bool
 
 	requestBuilder := fetcher.NewRequestBuilder()
 	requestBuilder.WithUsernameAndPassword(originalFeed.Username, originalFeed.Password)
-	requestBuilder.WithUserAgent(originalFeed.UserAgent)
+	requestBuilder.WithUserAgent(originalFeed.UserAgent, config.Opts.HTTPClientUserAgent())
 	requestBuilder.WithCookie(originalFeed.Cookie)
 	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
 	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())

internal/reader/processor/processor.go
@@ -65,7 +65,7 @@ func ProcessFeedEntries(store *storage.Storage, feed *model.Feed, user *model.Us
 			startTime := time.Now()
 
 			requestBuilder := fetcher.NewRequestBuilder()
-			requestBuilder.WithUserAgent(feed.UserAgent)
+			requestBuilder.WithUserAgent(feed.UserAgent, config.Opts.HTTPClientUserAgent())
 			requestBuilder.WithCookie(feed.Cookie)
 			requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
 			requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
@@ -166,7 +166,7 @@ func ProcessEntryWebPage(feed *model.Feed, entry *model.Entry, user *model.User)
 	websiteURL := getUrlFromEntry(feed, entry)
 
 	requestBuilder := fetcher.NewRequestBuilder()
-	requestBuilder.WithUserAgent(feed.UserAgent)
+	requestBuilder.WithUserAgent(feed.UserAgent, config.Opts.HTTPClientUserAgent())
 	requestBuilder.WithCookie(feed.Cookie)
 	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
 	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())

internal/ui/subscription_submit.go
@@ -60,7 +60,7 @@ func (h *handler) submitSubscription(w http.ResponseWriter, r *http.Request) {
 	requestBuilder := fetcher.NewRequestBuilder()
 	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
 	requestBuilder.WithProxy(config.Opts.HTTPClientProxy())
-	requestBuilder.WithUserAgent(subscriptionForm.UserAgent)
+	requestBuilder.WithUserAgent(subscriptionForm.UserAgent, config.Opts.HTTPClientUserAgent())
 	requestBuilder.WithCookie(subscriptionForm.Cookie)
 	requestBuilder.WithUsernameAndPassword(subscriptionForm.Username, subscriptionForm.Password)
 	requestBuilder.UseProxy(subscriptionForm.FetchViaProxy)