nilutz/miniflux-v2

commit b9bf6bc450e6a38895f1f19fd9f7baba358bf73a

Use a default parameter for goToPage as isn't ~always called with a single one

internal/ui/static/js/app.js
@@ -433,7 +433,7 @@ function unsubscribeFromFeed() {
  * @param {string} page Page to redirect to.
  * @param {boolean} fallbackSelf Refresh actual page if the page is not found.
  */
-function goToPage(page, fallbackSelf) {
+function goToPage(page, fallbackSelf = true) {
     const element = document.querySelector(":is(a, button)[data-page=" + page + "]");
 
     if (element) {

commit 4de9a15634e46f07c4d27adad403d364f205ff2c

fix(js): regression introduced in commit b9bf6bc
The default argument should be set to false.

internal/ui/static/js/app.js
@@ -433,7 +433,7 @@ function unsubscribeFromFeed() {
  * @param {string} page Page to redirect to.
  * @param {boolean} fallbackSelf Refresh actual page if the page is not found.
  */
-function goToPage(page, fallbackSelf = true) {
+function goToPage(page, fallbackSelf = false) {
     const element = document.querySelector(":is(a, button)[data-page=" + page + "]");
 
     if (element) {